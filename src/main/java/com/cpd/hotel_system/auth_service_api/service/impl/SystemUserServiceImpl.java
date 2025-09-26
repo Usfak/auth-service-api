2b0be310b5e6b4147536db0e851b6603073b04de
Usfak <mrashfak456@gmail.com>
2025-09-26 17:21:09 +0530

    keyclock and system user

---
+++ src/main/java/com/cpd/hotel_system/auth_service_api/service/impl/SystemUserServiceImpl.java
@@ -0,0 +1,144 @@
+package com.cpd.hotel_system.auth_service_api.service.impl;
+
+import com.amazonaws.services.amplify.model.BadRequestException;
+import com.cpd.hotel_system.auth_service_api.config.KeycloakSecurityUtil;
+import com.cpd.hotel_system.auth_service_api.dto.request.SystemUserRequestDto;
+import com.cpd.hotel_system.auth_service_api.entity.Otp;
+import com.cpd.hotel_system.auth_service_api.entity.SystemUser;
+import com.cpd.hotel_system.auth_service_api.exception.DuplicateEntryException;
+import com.cpd.hotel_system.auth_service_api.repo.OtpRepo;
+import com.cpd.hotel_system.auth_service_api.repo.SystemUserRepo;
+import com.cpd.hotel_system.auth_service_api.service.SystemUserService;
+import com.cpd.hotel_system.auth_service_api.util.OtpGenerator;
+import jakarta.ws.rs.core.Response;
+import lombok.RequiredArgsConstructor;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class SystemUserServiceImpl implements SystemUserService {
+
+
+    @Value("${keycloak.config.realm}")
+    private String realm;
+
+    private final OtpGenerator otpGenerator;
+    private final SystemUserRepo systemUserRepo;
+    private final KeycloakSecurityUtil keycloakUtil;
+    private final OtpRepo otpRepo;
+
+
+    @Override
+    public void createUser(SystemUserRequestDto dto) {
+        if (dto.getFirstName() == null || dto.getFirstName().trim().isEmpty()) {
+            throw new BadRequestException("First name is requird");
+        }
+
+        if (dto.getLastName() == null || dto.getLastName().trim().isEmpty()) {
+            throw new BadRequestException("Last name is requird");
+        }
+
+        if (dto.getEmail() == null || dto.getEmail().trim().isEmpty()) {
+            throw new BadRequestException("Email is requird");
+        }
+
+        String userId = "";
+        String otp = "";
+        Keycloak keycloak = null;
+
+        UserRepresentation existingUser = null;
+        keycloak = keycloakUtil.getKeycloakInstance();
+
+        existingUser = keycloak.realm(realm).users().search(dto.getEmail()).stream()
+                .findFirst().orElse(null);
+
+        if (existingUser != null) {
+            Optional<SystemUser> selectedSystemUserFromAuthService =
+                    systemUserRepo.findByEmail(dto.getEmail());
+            if (selectedSystemUserFromAuthService.isEmpty()) {
+                keycloak.realm(realm).users().delete(existingUser.getId());
+            } else {
+                throw new DuplicateEntryException("Email already exists");
+            }
+        } else {
+            Optional<SystemUser> selectedSystemUserFromAuthService =
+                    systemUserRepo.findByEmail(dto.getEmail());
+
+            if (selectedSystemUserFromAuthService.isPresent()) {
+                Optional<Otp> selectedOtp =
+                        otpRepo.findBySystemUserId(selectedSystemUserFromAuthService.get().getUserId());
+                if (selectedOtp.isPresent()) {
+                    otpRepo.deleteById(selectedOtp.get().getPropertyId());
+                }
+                systemUserRepo.deleteById(selectedSystemUserFromAuthService.get().getUserId());
+            }
+        }
+
+        UserRepresentation userRepresentation = mapUserRepo(dto,false,false);
+        Response response = keycloak.realm(realm).users().create(userRepresentation);
+        if (response.getStatus() == Response.Status.CREATED.getStatusCode()) {
+            RoleRepresentation userRole = keycloak.realm(realm).roles().get("user").toRepresentation();
+            userId = response.getLocation().getPath().replaceAll(".*/([^/]+)$", "$1");
+            keycloak.realm(realm).users().get(userId).roles().realmLevel().add(Arrays.asList(userRole));
+            UserRepresentation createdUser = keycloak.realm(realm).users().get(userId).toRepresentation();
+
+            SystemUser sUser = SystemUser.builder()
+                    .userId(userId)
+                    .keycloakId(createdUser.getId())
+                    .firstName(dto.getFirstName())
+                    .lastName(dto.getLastName())
+                    .email(dto.getEmail())
+                    .contact(dto.getContact())
+                    .isActive(false)
+                    .isAccountNonExpired(true)
+                    .isAccountNonLocked(true)
+                    .isCredentialsNonExpired(true)
+                    .isEnabled(false)
+                    .isEmailVerified(false)
+                    .createdAt(new Date().toInstant())
+                    .updatedAt(new Date().toInstant())
+                    .build();
+
+            SystemUser savedUser = systemUserRepo.save(sUser);
+            Otp createdOtp = Otp.builder()
+                    .propertyId(UUID.randomUUID().toString())
+                    .code(otpGenerator.generateOtp(5))
+                    .createdAt(Instant.now())
+                    .updatedAt(Instant.now())
+                    .isVerified(false)
+                    .systemUser(savedUser)
+                    .attempts(0)
+                    .build();
+            otpRepo.save(createdOtp);
+
+        }
+
+    }
+
+    private UserRepresentation mapUserRepo(SystemUserRequestDto dto, boolean isEmailVerified, boolean isEnabled) {
+        UserRepresentation user = new UserRepresentation();
+        user.setEmail(dto.getEmail());
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setUsername(dto.getEmail());
+        user.setEnabled(isEnabled);
+        user.setEmailVerified(isEmailVerified);
+        List<CredentialRepresentation> credList = new ArrayList<>();
+        CredentialRepresentation cred = new CredentialRepresentation();
+        cred.setTemporary(false);
+        cred.setValue(dto.getPassword());
+        credList.add(cred);
+        user.setCredentials(credList);
+        return user;
+    }
+
+}
